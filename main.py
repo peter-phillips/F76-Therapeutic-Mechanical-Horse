e553d5895ac9e736e74f9e9fd03211fda6b8cc06
ZMDominik <zuzia.dominik@gmail.com>
2022-05-12 13:21:47 -0700

    controler

--- main.py
+++ main.py
@@ -1,83 +1,134 @@
-import machine
-from wifi_manager import *
 from pin_manager import *
-from time import sleep
-try:
-    import usocket as socket
-except:
-    import socket
+from wifi_manager import *
 
 pin_manager = PinManager()
-wlan = get_connection()
+wifi_manager = WiFiManager()
 
-if wlan is None:
-    print("Could not initialize the network connection.")
-    while True:
-        pass  # you shall not pass :D
-
-# Main Code goes here, wlan is a working network.WLAN(STA_IF) instance.
-print("ESP OK")
-
-
-def web_page():
-    if pin_manager.led.value() == 1:
-        gpio_state = "ON"
-    else:
-        gpio_state = "OFF"
-
-    html = """<html><head> <title>ESP Web Server</title> <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="data:,"> <style>html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
-  h1{color: #0F3376; padding: 2vh;}p{font-size: 1.5rem;}.button{display: inline-block; background-color: #e7bd3b; border: none; 
-  border-radius: 4px; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
-  .button2{background-color: #4286f4;}</style></head><body> <h1>ESP Web Server</h1> 
-  <p>GPIO state: <strong>""" + gpio_state + """</strong></p><p><a href="/?led=on"><button class="button">ON</button></a></p>
-  <p><a href="/?led=off"><button class="button button2">OFF</button></a></p></body></html>"""
-    return html
-
-
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind(('', 80))
-    s.listen(5)
-except OSError as e:
-    machine.reset()
-
+horse_on = False
+just_changed = change_cyc = 1000
+pot_pos = 0
 while True:
-    try:
-        if gc.mem_free() < 102000:
-            gc.collect()
-        conn, addr = s.accept()
-        conn.settimeout(3.0)
-        print('Got a connection from %s' % str(addr))
-        request = conn.recv(1024)
-        conn.settimeout(None)
-        request = str(request)
-        print('Content = %s' % request)
-        led_on = request.find('/?led=on')
-        led_off = request.find('/?led=off')
-        if led_on == 6:
-            print('LED ON')
-            pin_manager.led.value(1)
-        if led_off == 6:
-            print('LED OFF')
-            pin_manager.led.value(0)
-        response = web_page()
-        conn.send('HTTP/1.1 200 OK\n')
-        conn.send('Content-Type: text/html\n')
-        conn.send('Connection: close\n\n')
-        conn.sendall(response)
-        conn.close()
-    except OSError as e:
-        conn.close()
-        print('Connection closed')
+    pot_pos = pin_manager.read_pot(pot_pos)
+    pressed = pin_manager.button_press()
 
-# set up all pins
-# pin_manager = PinManager()
-#
-# pot_pos = 0
-# while True:
-#     pot_pos = pin_manager.read_pot(pot_pos)
-#     pressed = pin_manager.button_press()
+    if just_changed == 0 and pressed:
+        if horse_on:
+            pin_manager.led.value(1)
+            wifi_manager.send_code("off_h")
+
+        if not horse_on:
+            pin_manager.led.value(0)
+            wifi_manager.send_code("on_h")
+
+        horse_on = not horse_on
+        just_changed = change_cyc
+
+    elif not pressed:
+        if just_changed > 0:
+            just_changed -= 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
